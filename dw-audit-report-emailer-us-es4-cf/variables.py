a6e0e62592fd1f8770b9f6907721efd16226dc11
Harshit Sharma <hsharma@toorakcapital.com>
2024-12-23 15:03:34 +0530

    feat: added audit report emailer

---
+++ dw-audit-report-emailer-us-es4-cf/variables.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+env = os.environ.get('stage', 'dev')
+project_id = os.environ.get('project_id', 'toorak-396910')
+sender_email = {"name":"noreply@toorakcapital.com","email":"noreply@toorakcapital.com"}
+
+if env == 'prod':
+    secret_name = {'email_api' : 'dw-noreply-email-api-key'}
+    
+    secret_project_id = 947624841920
+    
+else:
+    secret_name = {'email_api' : 'dw-np-noreply-email-api-key-sm-us-ct1'}
+    
+    secret_project_id = 280549663966
