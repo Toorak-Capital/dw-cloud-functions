a43332cb7b4edf5b9d674491596e4f8fab2d6be4
sasijyothsna <sasijyothsnap@gmail.com>
2025-01-20 18:33:41 +0530

    fix

--- dw-audit-report-emailer-us-es4-cf/variables.py
+++ dw-audit-report-emailer-us-es4-cf/variables.py
@@ -7,13 +7,13 @@ sender_email = {"name":"noreply@toorakcapital.com","email":"noreply@toorakcapita
 if env == 'prod':
     secret_name = {'email_api' : 'dw-noreply-email-api-key'}
 
-    email_type = 'Test'
+    email_type = ''
     
     secret_project_id = 947624841920
     
 else:
     secret_name = {'email_api' : 'dw-np-noreply-email-api-key-sm-us-ct1'}
 
-    email_type = ''
+    email_type = 'Test'
     
     secret_project_id = 280549663966
