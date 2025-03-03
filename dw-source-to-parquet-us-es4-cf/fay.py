c86a281edcb665b05ab266e46db37528e8093f4c
Praveen <praveenkumar.seetharaman@informa.com>
2025-03-03 20:08:58 +0530

    Added changes to process Fay servicer file

--- dw-source-to-parquet-us-es4-cf/fay.py
+++ dw-source-to-parquet-us-es4-cf/fay.py
@@ -1,29 +1,27 @@
 import re
 from variables import *
 from main import *
-import uuid
 from google.cloud import storage
-from datetime import datetime
+
 
 def extract_date(input_string):
     '''
     Extracts a date in the format mm.dd.yyyy and converts it to dd/mm/yy.
     '''
     # Regular expression pattern to match the date
-    pattern = r'\(?(\d{1,2})\.(\d{1,2})\.(\d{4})\)?'
+    pattern = r"(\d{2}\d{2}\d{2})"
     
     # Search for the first occurrence of the date pattern
     match = re.search(pattern, input_string)
     
+    # Check if a match is found
     if match:
-        # Extract day, month, and year
-        month, day, year = match.groups()
-
-        # Convert to dd/mm/yy format
-        formatted_date = f"{day}/{month}/{year[-2:]}"
-        return formatted_date
+        date_str = match.group()
+        # Convert to a date string
+        return datetime.strptime(date_str, '%m%d%y').strftime('%Y-%m-%d')
     else:
         return None
+    
     
 def rename_columns(df):
     try:
@@ -40,21 +38,6 @@ def rename_columns(df):
         print(f"An error occurred while renaming columns: {e}")
         return None
 
-# Mapping of file path patterns to subfolders
-FAY_SUBFOLDERS = {
-    "Fay_BK": "Fay_BK",
-    "Fay_BPO": "Fay_BPO",
-    "Fay_Comments": "Fay_Comments",
-    "Fay_COVID-19_Pandemic_Assistance": "Fay_COVID-19_Pandemic_Assistance",
-    "Fay_EDW_FC": "Fay_EDW_FC",
-    "Fay_Escrow": "Fay_Escrow",
-    "Fay_FCL": "Fay_FCL",
-    "Fay_LossMit": "Fay_LossMit",
-    "Fay_Modification": "Fay_Modification",
-    "Fay_Standard": "Fay_Standard",
-    "Fay_Supplemental": "Fay_Supplemental",
-    "Fay_TaxesInsurance": "Fay_TaxesInsurance",  # Default if none match
-}
 
 def trigger_on_fay_report(file_path, file_uri):
     """
@@ -75,9 +58,10 @@ def trigger_on_fay_report(file_path, file_uri):
     df = rename_columns(df)
 
     # Determine the correct subfolder dynamically
-    sub_folder = next((folder for folder in FAY_SUBFOLDERS if folder in file_path), "Fay_TaxesInsurance")
+    main_folder = 'Fay'
+    sub_folder = '_'.join(file_path.split('/')[-1].split('_')[:-1])
 
     # Write the DataFrame to Parquet
-    write_parquet_file(df, "Fay", sub_folder, formatted_date)
+    write_parquet_by_date(df, main_folder, sub_folder, formatted_date)
     
     print(f"Successfully wrote the Fay {sub_folder} Parquet file!")
