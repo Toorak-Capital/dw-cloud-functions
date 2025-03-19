b02a9a5507be79fbf0a76c537e013f2763be9c10
sasijyothsna <sasijyothsnap@gmail.com>
2025-03-19 13:00:26 +0530

    removing recipient

--- dw-daily-highlights-report-emailer-us-es4-function/variables.py
+++ dw-daily-highlights-report-emailer-us-es4-function/variables.py
@@ -25,8 +25,7 @@ if env == 'prod':
                         {"email":'sgoldman@toorakcapital.com',"name":"Scott Goldman"},
                         {"email":'cmacintosh@toorakcapital.com',"name":"Macintosh"},
                         {"email":'lberger@toorakcapital.com',"name":"Lincoln Berger"},
-                        {"email":'cschollmeyer@toorakcapital.com',"name":"Caryn Schollmeyer"},
-                        {"email":'kchen@toorakcapital.com',"name":"Kathy Chen"}]
+                        {"email":'cschollmeyer@toorakcapital.com',"name":"Caryn Schollmeyer"}]
     
     email_recipients_cc = [{"email":'toorakdatateam@triconinfotech.com',"name":"Data Team"}]
     
